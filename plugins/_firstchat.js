713f99bcada771ecee8bfbde2c2c05fbe3091256
Putbotz <96361236+Putbotz@users.noreply.github.com>
2023-02-05 00:20:11 +0800

    Update _firstchat.js

--- plugins/_firstchat.js
+++ plugins/_firstchat.js
@@ -6,10 +6,10 @@ export async function before(m) {
     let user = global.db.data.users[m.sender]
     let txt = `👋Hai, ${ucapan()}
 
-${user.banned ? '📮Maaf, kamu dibanned & Tidak bisa menggunakan bot ini lagi' : `💬 Ada yg bisa ${this.user.name} bantu?`}`.trim()
+${user.banned ? '📮Maaf, kamu dibanned & Tidak bisa menggunakan bot ini lagi' : `put tngh busy/tido jangan spam`}`.trim()
 
     if (new Date() - user.pc < 21600000) return // waktu ori 21600000 (6 jam)
-    await this.sendButton(m.chat, txt, user.banned ? wm : '📮Note: Jangan spam bot nya', [user.banned ? 'OWNER' : 'OWNER', user.banned ? '.owner' : '.owner'], m)
+    await this.sendButton(m.chat, txt, user.banned ? wm : '© Putbotz', [user.banned ? 'OWNER' : 'OWNER', user.banned ? '.owner' : '.owner'], m)
     user.pc = new Date * 1
 }
 
